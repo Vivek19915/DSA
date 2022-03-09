d477cbb3d81aa76da24ad080b81c62c059c6178a
Vivek19915 <vchoudhary19915@gmail.com>
2022-03-09 11:08:39 +0530

    .

--- Heap/most_closet_point_to_origin.cpp
+++ Heap/most_closet_point_to_origin.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     // four points are given
-    int points[4][2] = {{1, 1}, {2, 1}, {3, 18}, {1, 2}};
+    int points[5][2] = {{1, 1}, {2, 1}, {3, 18}, {1, 2}, {0, 1}};
     // here we take the conkey of key
     // and we do it by using min heap kyuki hame ek hi sabse
     // smallest key inkalini jo ki heap heap ke top par hogi
@@ -15,7 +15,7 @@ int main()
     // creating min heap
     priority_queue<pair< int , pair< int , int > >, vector<pair< int , pair< int , int > > >, greater<pair< int , pair< int , int > > > > minheap;
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 5; i++)
     {
         // key =  distance bet point and origin
         int key = (points[i][0]) * (points[i][0]) + (points[i][1]) * (points[i][1]);
